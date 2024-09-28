d4fccfebffb0816f2e72c8a920af2758e5395252
Ansalpandey <pandeyansal77@gmail.com>
2024-09-28 13:23:09 +0530

    chore: Add analytics to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -29,6 +30,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          {/* Include Analytics */}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
